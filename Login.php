c880da09e81776f052e2131b7c3c8d78a63968f5
Felipe Biava <valdivinobiava496@gmail.com>
2022-07-19 14:20:16 -0300

    mudei o css e coloquei o p junto do card-footer a{ }

--- Login.php
+++ Login.php
@@ -49,7 +49,7 @@ BACKGROUND-IMAGE: url(imagens/ai.jpg)
 
     <div class="card-footer">
         <input type="submit" value="login" class="submit">
-        <a href="logarse.php">Não possui uma conta?<u> Clique aqui!</u></a>
+        <p>Não possui uma conta?<u> <a href="logarse.php">Clique aqui!</u></a></p>
     </div>
 
 </form>
