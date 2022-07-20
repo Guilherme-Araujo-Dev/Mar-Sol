818f67999d2633acd52bf1059d1722bf14b499a7
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-07-20 17:03:21 -0300

    Header e Footer em arquivos separados

---
+++ PHP/header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" type="text/css" href="../CSS/style-header.css">
+
+<header>
+        <nav class="navbar navbar-expand-lg color-header fixed-top p-2">
+            <a class="navbar-brand" href="#">
+                <img src="../IMG/logo.png" alt="" width="80" height="60">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end fs-4" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Início</a>
+                    <a class="nav-link" href="#">Produtos</a>
+                    <a class="nav-link" href="#">Sobre</a>
+                </div>
+            </div>
+        </nav>
+</header>
+
