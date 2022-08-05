930f794781c1b64db834b1f32af0f60f75c427f4
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-08-04 23:33:18 -0300

    Arrumando o Header

--- JS/select-page.js
+++ JS/select-page.js
@@ -1,6 +1,9 @@
+// Isso é só uma solução Provisória
+
 navHome = document.getElementById("home");
 navProduct = document.getElementById("product");
 navAbout = document.getElementById("about");
+navMakeRequest = document.getElementById("makeRequest");
 
 window.onload = function() {
     if(navHome.classList.contains("active")) {
@@ -9,6 +12,8 @@ window.onload = function() {
         navProduct.classList.remove("active");
     } else if(navAbout.classList.contains("active")) {
         navAbout.classList.remove("active");
+    } else if(navMakeRequest.classList.contains("active")) {
+        navMakeRequest.classList.remove("active");
     }
 
     if(document.title == "Mar & Sol Salgados - Início") {
@@ -17,5 +22,7 @@ window.onload = function() {
         navProduct.classList.add("active");
     } else if(document.title == "Mar & Sol Salgados - Sobre") {
         navAbout.classList.add("active");
+    } else if(document.title == "Mar & Sol Salgados - Fazer Pedido") {
+        navMakeRequest.classList.add("active");
     }
 }
