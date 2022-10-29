19e8abea71f03452abbc8b3c3fd6b67294e50535
Guilherme Araujo <guilhermearaujop26@gmail.com>
2022-10-29 14:58:44 -0300

    Corrigindo Bug no Produtos

--- PHP/user/product.php
+++ PHP/user/product.php
@@ -47,25 +47,25 @@
         <?php
         if (isset($_POST['btnBusca'])) {
             $busca    = isset($_POST['busca']) ? $_POST['busca'] : ' ';
-            $sql = "SELECT * FROM produtos WHERE nomeProduto LIKE ?";
+            $sql = "SELECT * FROM produto WHERE nomeProduto LIKE ?";
             $stmt = $pdo->prepare($sql);
             $buscaGeral = '%' . $busca . '%';
             $stmt->bindParam(1, $buscaGeral);
         } else {
-            $sql = "SELECT * FROM produtos LIMIT 10";
+            $sql = "SELECT * FROM produto LIMIT 10";
             $stmt = $pdo->prepare($sql);
         }
         // preparando o sql para não aceitar sql injection
         $stmt->execute();
         
         // pegando todos os dados da tabela
-        $produtos = $stmt->fetchAll();
+        $produto = $stmt->fetchAll();
         ?>
         <br><br><br><br>
 
         <!--BARRA DE PESQUISA-->
         <ul class="produtos">
-        <?php foreach ($produtos as $p) { ?>
+        <?php foreach ($produto as $p) { ?>
                 <li class="produto">
                     <img src="../../IMG/food/<?php echo $p['imagem']; ?>" alt="<?php echo $p['nomeProduto']; ?>" width="300px" height="167px">
                     <h1 class="product-text"><?php echo $p['nomeProduto']; ?></h1>
