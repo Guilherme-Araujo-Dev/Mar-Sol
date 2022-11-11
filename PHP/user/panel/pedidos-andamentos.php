88f6bb910d43e417ecde19ba4828c9ff0a368a70
Felipe Biava <valdivinobiava496@gmail.com>
2022-11-11 17:35:56 -0300

    perfil feito

---
+++ PHP/user/panel/pedidos-andamentos.php
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Importando o BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+
+    <!-- Definindo o Ícone da Página -->
+    <link rel="shortcut icon" href="../../../IMG/favicon.ico" type="image/x-icon" />
+    
+    <!-- Importando o CSS do sidebar-->
+    <link rel="stylesheet" type="text/css" href="../../../CSS/style-adm.css">
+
+   <!-- Importando o CSS-->
+   <link rel="stylesheet" href="../../../CSS/style-pedidos-andamentos.css.css">
+
+    <title>Mar & Sol - Painel do Administrador</title>
+</head>
+
+<body>
+
+    <div class="sidebar">
+
+        <header>PERFIL</header>
+        <ul>
+            <li>
+                <a href="#"> Pedidos Encaminhados</a>
+            </li>
+
+            <li>
+                <a href="../panel/pedidos-entregar.php"> Pedidos à Entregar</a>
+            </li>
+
+            <li>
+                <a href="../panel/pedidos-entregues.php"> Pedidos Entregues</a>
+            </li>
+
+            <li>
+                <a href="../panel/pedidos-cancelados.php"> Pedidos Cancelados</a>
+            </li>
+
+            <!--
+            <li class="voltar">
+                <a href="../adm/admPRODUCT.php"><i class="fa-solid fa-arrow-left"></i> Voltar</a>
+            </li>
+            -->
+        </ul>
+    </div>
+</head>
+
+<body>
+<center><br>
+        <div class="admREPORT">
+            <h1>Encaminhados</h1>
+        </div>
+    </center>
+    <br><br>
+
+    <div class="small-container cart-page">
+        <table>
+            <tr>
+                <th>Produto</th>
+                <th class="invisivel">invisivel</th>
+                <th class="invisivel">invisivel</th>
+                <th>Nome do Produto</th>
+            </tr>
+
+
+                <td>
+                    <div>
+                        <img src="../../../IMG/food/ppg.jpg" alt="imagem" width="100px" height="100px">
+                    </div>
+                </td>
+
+                <td>
+                    <div>
+                        <p>
+                            
+                        </p>
+                    </div>
+                </td>
+
+                <td>
+                    <div>
+                        <p>
+                            
+                        </p>
+                    </div>
+                </td>
+
+                <td>
+                    <div>
+                        <p>
+                            Coxinha de Carne Grande
+                        </p>
+                    </div>
+                </td>
+
+            </tr>
+            <!--
+                1. O tr cria uma nova linha dentro da tabela
+                2. Para colocar novos textos NA MESMA LINHA tem que usar o td dentro do tr
+            -->
+        </table>
+    </div>
+
+<script src="https://kit.fontawesome.com/a8239b02c3.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ PHP/user/panel/profile.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Importando o BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+
+    <!-- Definindo o Ícone da Página -->
+    <link rel="shortcut icon" href="../../../IMG/favicon.ico" type="image/x-icon" />
+    
+    <!-- Importando o CSS do sidebar-->
+    <link rel="stylesheet" type="text/css" href="../../../CSS/style-adm.css">
+
+    <!-- Importando o CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../CSS/style-profile.css">
+
+
+    
+
+    <title>Mar & Sol - Painel do Administrador</title>
+</head>
+<body>
+
+    <div class="sidebar">
+        <header>Perfil</header>
+        <ul>
+            <li>
+                <a href="../panel/pedidos-andamentos.php"> Pedidos em Andamento</a>
+            </li>
+
+            <li>
+                <a href=""> Pedidos à entregar</a>
+            </li>
+
+            <li>
+                <a href=""> Pedidos Entregues</a>
+            </li>
+
+            <li>
+                <a href=""> Pedidos Cancelados</a>
+            </li>
+
+            <!--
+            <li class="voltar">
+                <a href="../user/index.php"><i class="fa-solid fa-arrow-left"></i> Voltar ao Site</a>
+            </li>
+            -->
+
+        </ul>
+    </div>
+
+    <center>
+        <br>
+            <svg xmlns="http://www.w3.org/2000/svg" width="410" height="410" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
+        
+        <br><br>
+    </center>
+
+<!--Importando o bootstrap-->
+
+<script src="https://kit.fontawesome.com/a8239b02c3.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
